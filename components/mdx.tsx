7cae23ee893054b6d9d72700649d55f32b8e356a
maxcabd <max.cabd@gmail.com>
2025-01-23 11:29:22 -0500

    Fix markdown formatting not rendering in mdx.ts

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,6 +1,68 @@
-'use client';
+"use client";
 
-import { useMDXComponent } from 'next-contentlayer/hooks';
+import { useMDXComponent } from "next-contentlayer/hooks";
+import Link from "next/link";
+
+const components = {
+  h1: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h1
+      className="mt-2 scroll-m-20 text-4xl font-bold tracking-tight"
+      {...props}
+    />
+  ),
+  h2: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h2
+      className="mt-10 scroll-m-20 border-b pb-1 text-3xl font-semibold tracking-tight first:mt-0"
+      {...props}
+    />
+  ),
+  h3: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h3
+      className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight"
+      {...props}
+    />
+  ),
+  h4: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h4
+      className="mt-8 scroll-m-20 text-xl font-semibold tracking-tight"
+      {...props}
+    />
+  ),
+  p: ({ className, ...props }: React.HTMLAttributes<HTMLParagraphElement>) => (
+    <p className="leading-7 [&:not(:first-child)]:mt-6" {...props} />
+  ),
+  ul: ({ className, ...props }: React.HTMLAttributes<HTMLUListElement>) => (
+    <ul className="my-6 ml-6 list-disc" {...props} />
+  ),
+  ol: ({ className, ...props }: React.HTMLAttributes<HTMLOListElement>) => (
+    <ol className="my-6 ml-6 list-decimal" {...props} />
+  ),
+  li: ({ className, ...props }: React.HTMLAttributes<HTMLLIElement>) => (
+    <li className="mt-2" {...props} />
+  ),
+  blockquote: ({
+    className,
+    ...props
+  }: React.HTMLAttributes<HTMLQuoteElement>) => (
+    <blockquote
+      className="mt-6 border-l-2 pl-6 italic [&>*]:text-muted-foreground"
+      {...props}
+    />
+  ),
+  a: ({ className, ...props }: React.HTMLAttributes<HTMLAnchorElement>) => (
+    <Link
+      className="font-medium underline underline-offset-4"
+      {...props}
+      href={props.href ?? "#"}
+    />
+  ),
+  code: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
+    <code
+      className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold"
+      {...props}
+    />
+  ),
+};
 
 interface MdxProps {
   code: string;
@@ -8,5 +70,9 @@ interface MdxProps {
 
 export function Mdx({ code }: MdxProps) {
   const Component = useMDXComponent(code);
-  return <Component />;
+  return (
+    <div className="mdx">
+      <Component components={components} />
+    </div>
+  );
 }
